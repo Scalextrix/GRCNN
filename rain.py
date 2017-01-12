2dd3f4668aaa3c6b50cf52607cd844f4e1e0f4fa
Scalextrix <steven.campbell100@gmail.com>
2017-01-12 16:18:42 +0000

    Create rain.py

---
+++ rain.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Gather list of Whitelisted projects from Gridcoin NN, call each project for team member CPIDs,
+allow user to send a multi transaction to rain GRC on team members"""
+
+import getpass
+import subprocess
+from urllib2 import urlopen
+import sqlite3
+import gc
+import xml.etree.ElementTree as ET
+
+gridcoin_passphrase = getpass.getpass(prompt="What is your Gridcoin Wallet Passphrase: ")
+
+rosetta_url = ("https://boinc.bakerlab.org/rosetta/team_email_list.php?teamid=12575&account_key=Y&xml=1")
+project = urlopen(rosetta_url)
+
+root = ET.parse(urllib.urlopen(rosetta_url)).getroot()
+print root
+print '\n'
+
+items = root.findall('items/item')
+for item in items: 
+     item.find('cpid').text 
