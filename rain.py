0f8aff2b4187c5ee518063ab746832d337beb6ae
Scalextrix <steven.campbell100@gmail.com>
2017-01-14 15:02:32 +0000

    Update rain.py

--- rain.py
+++ rain.py
@@ -8,6 +8,7 @@ from urllib2 import urlopen
 import gc
 import xml.etree.ElementTree as ET
 import subprocess
+import sys
 
 gridcoin_passphrase = getpass.getpass(prompt="What is your Gridcoin Wallet Passphrase: ")
 grc_amount = raw_input("How much GRC to rain on each project CPID: ")
@@ -15,28 +16,47 @@ grc_amount = raw_input("How much GRC to rain on each project CPID: ")
 
 rosetta_url = ("https://boinc.bakerlab.org/rosetta/team_email_list.php?teamid=12575&account_key=Y&xml=1")
 
-root = ET.parse(urlopen(rosetta_url)).getroot()
-cpids = [el.text for el in root.findall('.//user/cpid')]
-cpids = zip(*[iter(cpids)]*1)
+def xml_to_db():
+    rain_team = raw_input("Which BOINC project to RAIN on: ").lower()
+    if rain_team == "rosetta" or rain_team == "rosetta@home":
+        project_url = rosetta_url
+    else:
+        sys.exit("Sorry: BOINC Team not recognised")
+        
+    root = ET.parse(urlopen(project_url)).getroot()
+    cpids = [el.text for el in root.findall('.//user/cpid')]
+    rac = [el.text for el in root.findall('.//user/expavg_credit')]
+    cpids = zip(*[iter(cpids), iter(rac)]*1)
+    print rac
 
-conn = sqlite3.connect("GridcoinTeam.db")
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS GRIDCOINTEAM (cpid TEXT)''') 
-c.executemany("INSERT INTO GRIDCOINTEAM VALUES (?);", cpids)
-conn.commit()		
-conn.close()
+    conn = sqlite3.connect("GridcoinTeam.db")
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS GRIDCOINTEAM (cpid TEXT, rac TEXT)''') 
+    c.executemany("INSERT INTO GRIDCOINTEAM VALUES (?,?);", cpids)
+    conn.commit()		
+    conn.close()
 
-conn = sqlite3.connect("GridcoinTeam.db")
-c = conn.cursor()
-cpids = c.execute('select cpid from GRIDCOINTEAM').fetchall()
-conn.close()
+    conn = sqlite3.connect("GridcoinTeam.db")
+    c = conn.cursor()
+    cpids = c.execute('select cpid from GRIDCOINTEAM').fetchall()
+    rac = c.execute('select rac from GRIDCOINTEAM').fetchall()
+    conn.close()
 
-print cpids
-     
-     
-subprocess.call(['gridcoinresearchd', 'walletlock'], shell=False)
-subprocess.call(['gridcoinresearchd', 'walletpassphrase', gridcoin_passphrase, '9999999'], shell=False)
-subprocess.call(['gridcoinresearchd', 'sendmany', addresses, grc_amount, '', '', 'Its raining GRC'], shell=False)
-subprocess.call(['gridcoinresearchd', 'walletlock'], shell=False)
-subprocess.call(['gridcoinresearchd', 'walletpassphrase', gridcoin_passphrase, '9999999', 'true'], shell=False)
+def db_clean():
+    conn = sqlite3.connect("GridcoinTeam.db")
+    c = conn.cursor()
+    c.execute('''DROP TABLE IF EXISTS GRIDCOINTEAM''') 
+    conn.commit()		
+    conn.close()
+
+def execute_send():     
+     subprocess.call(['gridcoinresearchd', 'walletlock'], shell=False)
+     subprocess.call(['gridcoinresearchd', 'walletpassphrase', gridcoin_passphrase, '9999999'], shell=False)
+     subprocess.call(['gridcoinresearchd', 'sendmany', addresses, grc_amount, '', '', 'Its raining GRC'], shell=False)
+     subprocess.call(['gridcoinresearchd', 'walletlock'], shell=False)
+     subprocess.call(['gridcoinresearchd', 'walletpassphrase', gridcoin_passphrase, '9999999', 'true'], shell=False)
+
+xml_to_db()
+execute_send()
+db_clean()
 gc.collect()
