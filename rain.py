d9c7044f04d2729972b3c1fcf0037e9948ce251d
Scalextrix <steven.campbell100@gmail.com>
2017-01-24 16:09:20 +0000

    more projects

--- rain.py
+++ rain.py
@@ -24,12 +24,17 @@ filename = "C:\\Users\\%s\\AppData\\Roaming\\GridcoinResearch\\reports\\DailyNeu
 
 asteroids_url = ("http://asteroidsathome.net/boinc/team_email_list.php?teamid=2218&xml=1")
 atlas_url = ("http://atlasathome.cern.ch/team_email_list.php?teamid=1869&account_key=Y&xml=1")
+burp_url = ("https://burp.renderfarming.net/team_email_list.php?teamid=1285&xml=1")
 collatz_url = ("http://boinc.thesonntags.com/collatz/team_email_list.php?teamid=3029&xml=1")
+cosmology_url = ("http://www.cosmologyathome.org/team_email_list.php?teamid=3637&xml=1")
 csgrid_url = ("http://csgrid.org/csg/team_email_list.php?teamid=154&xml=1")
 ddm_url = ("http://www.distributeddatamining.org/DistributedDataMining/team_email_list.php?teamid=2176&xml=1")
+einstein_url = ("https://einsteinathome.org/team_email_list.php?teamid=13630&account_key=Y&xml=1")
+enigma_url = ("http://www.enigmaathome.net/team_email_list.php?teamid=2937&xml=1")
+find_url = ("http://findah.ucd.ie/team_email_list.php?teamid=2198&xml=1)
 gpugrid_url = ("https://www.gpugrid.net/team_email_list.php?teamid=3493&xml=1")
 gridcoin_finance_url = ("finance.gridcoin.us/finance/team_email_list.php?teamid=5&xml=1")
-einstein_url = ("https://einsteinathome.org/team_email_list.php?teamid=13630&account_key=Y&xml=1")
+leiden_url = ("http://boinc.gorlaeus.net/team_email_list.php?teamid=1629&xml=1")
 lhc_url = ("http://lhcathomeclassic.cern.ch/sixtrack/team_email_list.php?teamid=8128&xml=1")
 milkyway_url = ("http://milkyway.cs.rpi.edu/milkyway/team_email_list.php?teamid=6566&xml=1")
 poem_url = ("http://boinc.fzk.de/poem/team_email_list.php?teamid=3147&xml=1")
@@ -42,39 +47,49 @@ yafu_url = ("http://yafu.myfirewall.org/yafu/team_email_list.php?teamid=260&acco
 
 rain_team = raw_input("Which BOINC project to RAIN on: ").lower()
 if rain_team == "asteroids" or rain_team == "asteroids@home":
-	project_url = asteroids_url
+	    project_url = asteroids_url
 elif rain_team == "atlas" or rain_team == "atlas@home":
-	project_url = atlas_url
+	    project_url = atlas_url
+elif rain_team == "burp":
+	    project_url = burp_url
 elif rain_team == "collatz" or rain_team == "collatz conjecture":
-	project_url = collatz_url
+	    project_url = collatz_url
+elif rain_team == "cosmology" or rain_team == "cosmology@home":
+	    project_url = cosmology_url
 elif rain_team == "csg" or rain_team == "csgrid" or rain_team == "citizen science grid":
-	project_url = csgrid_url
+	    project_url = csgrid_url
 elif rain_team == "ddm" or rain_team == "distributed data mining":
-	project_url = ddm_url
-elif rain_team == "gpugrid" or rain_team == "gpugrid.net":
-	project_url = gpugrid_url
-elif rain_team == "gridcoin finance":
-	project_url = gridcoin_finance_url
+	    project_url = ddm_url
 elif rain_team == "einstein" or rain_team == "einstein@home":
-	project_url = einstein_url
+	    project_url = einstein_url
+elif rain_team == "enigma" or rain_team == "enigma@home"
+	    project_url = enigma_url
+elif rain_team == "find" or rain_team == "find@home":
+	    project_url = find_url
+elif rain_team == "gpugrid" or rain_team == "gpugrid.net":
+	    project_url = gpugrid_url
+elif rain_team == "gridcoin finance":
+	    project_url = gridcoin_finance_url
+elif rain_team == "leiden" or rain_team == "leiden@home":
+	    project_url = leiden_url
 elif rain_team == "lhc" or rain_team == "lhc@home":
-	project_url = lhc_url
+	    project_url = lhc_url
 elif rain_team == "milkyway" or rain_team == "milky way" or rain_team == "milkyway@home":
-	project_url = milkyway_url
+	    project_url = milkyway_url
 elif rain_team == "poem" or rain_team == "poem@home":
-	project_url = poem_url
+	    project_url = poem_url
 elif rain_team == "primegrid" or rain_team == "primegrid@home":
-	project_url = primegrid_url
+	    project_url = primegrid_url
 elif rain_team == "rosetta" or rain_team == "rosetta@home":
-	project_url = rosetta_url
+	    project_url = rosetta_url
 elif rain_team == "seti" or rain_team == "seti@home":
-	project_url = seti_url
+	    project_url = seti_url
 elif rain_team == "wcg" or rain_team == "world community grid":
-	project_url = wcg_url
+	    project_url = wcg_url
 elif rain_team == "yafu" or rain_team == "yafu@home":
-	project_url = yafu_url
+	    project_url = yafu_url
 else:
-	sys.exit("Sorry: BOINC Team not recognised")
+	    sys.exit("Sorry: BOINC Team not recognised")
 
 grc_amount = float(raw_input("How much GRC to rain on BOINC project: "))
 account_label = raw_input("Choose Wallet Account Label from which the GRC should be taken: ")
